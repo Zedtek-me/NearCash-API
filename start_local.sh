bbb42f0ef86b9059283676694dda0cdd99ee9b2a
Zechariah <zechariahadebayo42@gmail.com>
2025-06-01 01:10:29 +0100

    chore: updated shell script and graphql folders

--- start_local.sh
+++ start_local.sh
@@ -4,4 +4,4 @@ echo "running migrations"
 python3 -m manage makemigrations --noinput
 python3 -m manage migrate --noinput
 echo "starting server..."
-python3 -m manage runserver 3000
+python3 -m manage runserver 0.0.0.0:3000
