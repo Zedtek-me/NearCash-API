29e9e6b7ab7416c75eced1a454743c869f190846
Zechariah <zechariahadebayo42@gmail.com>
2025-06-11 00:31:41 +0100

    chore: not implemented duplicate error fix

--- interfaces/auths/interface.py
+++ interfaces/auths/interface.py
@@ -3,25 +3,25 @@ from typing import Union, Optional
 
 class AuthInterface(ABC):
     """to be implemented by all auth services"""
-
+    NOT_IMPLEMENTED = "method not implemented!"
     @classmethod
     @abstractmethod
     def get_auth_url(cls) -> Optional[str]:
         """
         Returns the OAuth2 authorization URL.
         """
-        raise NotImplementedError("method not implemented!")
+        raise NotImplementedError(cls.NOT_IMPLEMENTED)
 
     @classmethod
     @abstractmethod
     def get_auth_tokens(cls, auth_code) -> Union[dict, str, None]:
         """fetches auth tokens with auth code"""
-        raise NotImplementedError("method not implemented!")
-    
+        raise NotImplementedError(cls.NOT_IMPLEMENTED)
+
     @classmethod
     @abstractmethod
     def fetch_user_info(
         cls, credentials: dict
     ) -> Union[dict, str, list]:
         """fetches user information with $credentials"""
-        raise NotImplementedError("method not implemented!")
+        raise NotImplementedError(cls.NOT_IMPLEMENTED)
