f61bbb931ff41bdc3fb5097e2672409392df444e
Zechariah <zechariahadebayo42@gmail.com>
2025-07-08 22:41:39 +0100

    chore: financial asset info during business creation

--- apps/core/schema/mutations/business_mutations.py
+++ apps/core/schema/mutations/business_mutations.py
@@ -2,8 +2,12 @@ import graphene
 from graphql_jwt.decorators import login_required
 from django.db import transaction
 from apps.core.schema.types.business_types import BusinessType, CreateBusinessInputType
+from apps.wallet.schema.types.wallet import (
+    AssetInputType,
+)
 
 from utils.core_utils.business_utils import BusinessUtil
+from utils.wallet_utils.wallet import WalletUtil
 
 
 class CreateBusiness(graphene.Mutation):
@@ -14,6 +18,9 @@ class CreateBusiness(graphene.Mutation):
 
     class Arguments:
         data = CreateBusinessInputType(required=True)
+        financial_assets = graphene.List(
+            AssetInputType, required=False
+        )
 
     @login_required
     @transaction.atomic
@@ -22,8 +29,13 @@ class CreateBusiness(graphene.Mutation):
 
         user = info.context.user
         data = kwargs.get("data", {})
+        financial_assets = kwargs.get("financial_assets", [])
         business_data = UserUtil.prepare_business_data(data)
         business = BusinessUtil.create_business(user, business_data)
+        if financial_assets:
+            WalletUtil.create_financial_assets(
+                business, financial_assets
+            )
         return CreateBusiness(
             message="Business created successfully.",
             business=business
