710b1ece67b497b7800ba71aa2d6c41adb94cd00
Zechariah <zechariahadebayo42@gmail.com>
2025-10-06 20:10:07 +0100

    chore: client notif

--- utils/wallet_utils/transactions.py
+++ utils/wallet_utils/transactions.py
@@ -3,6 +3,12 @@ import uuid
 from django.db.models import Q
 
 from apps.wallet.models import Transaction
+from utils.helpers.exception import CustomException
+
+from apps.wallet.constants import (
+    IN_PROGRESS, CANCELLED,
+    FULFILLED, TXN_STATUSES
+)
 
 class TransactionUtil:
     """all things txn related"""
@@ -39,3 +45,27 @@ class TransactionUtil:
         updates txn status and publish notification accordingly, if needed --
         depending on the status type.
         """
+        from background_tasks.core.business import BusinessAsyncOperations
+
+        txn = cls.get_transaction(
+            id=data.get("txn_id"), only_one=True
+        )
+        if not txn:
+            raise CustomException("Transaction not found.")
+
+        status = data.get("status", "")
+        if status and not isinstance(status, str):
+            status = status.value.upper()
+
+        if status == IN_PROGRESS and user.id != txn.vendor.id:
+            raise CustomException(
+                "Only the vendor can update the transaction to IN_PROGRESS."
+            )
+        txn.status = status
+        txn.save()
+        # tell client that the vendor has accepted the txn, and has started processing it already.
+        if status == IN_PROGRESS:
+            BusinessAsyncOperations.notify_client_of_txn_status.delay(
+                txn_id=txn.id
+            )
+        return txn
