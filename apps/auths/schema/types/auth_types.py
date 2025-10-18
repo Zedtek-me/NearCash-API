acf4245062a4a73710216e6bc92bbc56d439e0cf
Zechariah <zechariahadebayo42@gmail.com>
2025-10-18 09:50:00 +0100

    chore: full name resolution added to user type

--- apps/auths/schema/types/auth_types.py
+++ apps/auths/schema/types/auth_types.py
@@ -6,15 +6,21 @@ from apps.auths.models import User
 from apps.auths.schema.types.enums import UserTypeEnum
 from apps.core.schema.types.business_types import CreateBusinessInputType
 
+from utils.helpers.logs import logger
+
 
 class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = "__all__"
     user_type = graphene.String()
+    full_name = graphene.String()
 
     def resolve_user_type(self, info):
         return self.meta.get("user_type", "CLIENT")
+
+    def resolve_full_name(self, info):
+        return self.full_name
 
 
 
