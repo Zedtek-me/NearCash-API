9e1297e5cba8d1619032ff7e60d80a9eddb5fe9c
Zechariah <zechariahadebayo42@gmail.com>
2026-02-07 21:07:16 +0100

    chore: staging deployment update

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
+#!bin/sh
 
+echo "running migrations"
+python3 -m manage migrate --noinput
+echo "starting server..."
+gunicorn near_cash.wsgi:application --bind 0.0.0.0:3000 --workers 4 \
+    --worker-class gevent --timeout 120 --keep-alive 5 \
+    --log-level info --access-logfile - --error-logfile -
