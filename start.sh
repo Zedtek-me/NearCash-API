01e56cd6834ed60e7d46fcfdc5a754309c4ccf38
Zechariah <zechariahadebayo42@gmail.com>
2026-02-07 22:11:35 +0100

    static files collections script

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 echo "running migrations"
 python3 -m manage migrate --noinput
+python3 -m manage collectstatic --noinput --clear --link
 echo "starting server..."
 gunicorn near_cash.wsgi:application --bind 0.0.0.0:3000 --workers 4 \
     --worker-class gevent --timeout 120 --keep-alive 5 \
