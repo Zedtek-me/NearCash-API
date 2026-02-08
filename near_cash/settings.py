ba69e4e2bf4164a7e7d9815af0b90d29a2f542fd
Zechariah <zechariahadebayo42@gmail.com>
2026-02-08 09:55:36 +0100

    chore: added whitenoise middleware

--- near_cash/settings.py
+++ near_cash/settings.py
@@ -67,9 +67,9 @@ CORS_ALLOWED_METHODS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
