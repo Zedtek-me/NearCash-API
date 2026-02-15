143987618c5a96ef06e9a348c9a76ffd9cb48716
Zechariah <zechariahadebayo42@gmail.com>
2026-02-15 05:52:11 +0100

    deployment server update 2

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ echo "running migrations"
 python3 -m manage migrate --noinput
 python3 -m manage collectstatic --noinput --clear --link
 echo "starting server..."
-daphne near_cash.asgi:application --bind 0.0.0.0:3000 --workers 4 \
-    --worker-class gevent --timeout 120 --keep-alive 5 \
-    --log-level info --access-logfile - --error-logfile -
+daphne near_cash.asgi:application --bind 0.0.0.0:3000
+
+gunicorn extra deployment args
+# --workers 4 \
+#     --worker-class gevent --timeout 120 --keep-alive 5 \
+#     --log-level info --access-logfile - --error-logfile -
