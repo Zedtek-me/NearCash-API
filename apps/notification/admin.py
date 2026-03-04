1a166a5651ff2f50489f5996f265e083a301afa3
Zechariah <zechariahadebayo42@gmail.com>
2026-03-04 23:38:19 +0100

    chore: admin update

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -11,15 +11,16 @@ class NotificationAdmin(admin.ModelAdmin):
         "date_created", "last_updated"
     ]
     list_filter = [
-        "date_created", "last_updated", "content_type"
+        "date_created", "last_updated", "status"
     ]
     search_fields = [
-        "title__icontains", "message__icontains", "content_type__model__icontains"
+        "title__icontains", "message__icontains", "content_type__model__icontains",
+        "status__icontains"
     ]
     readonly_fields = [
         "date_created", "last_updated", "content_type", "object_id", "title", "message"
     ]
     fields = [
-        "title", "message", "content_type", "object_id", "status", "date_created",
-        "last_updated", "meta"
+        "title", "message", "content_type", "object_id", "status", "meta"
+        "date_created", "last_updated"
     ]
