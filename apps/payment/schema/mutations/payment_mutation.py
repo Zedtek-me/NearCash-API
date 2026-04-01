e326de70e9cfb30c5d4547f9b2fc39009a253cde
Zechariah <zechariahadebayo42@gmail.com>
2026-04-01 13:23:41 +0100

    chore: account info regeneration

--- apps/payment/schema/mutations/payment_mutation.py
+++ apps/payment/schema/mutations/payment_mutation.py
@@ -26,7 +26,7 @@ class GenerateVirtualAccount(graphene.Mutation):
                 message=f"Invalid transaction id: {trxn_id}!"
             )
         current_virtual_account_info = trxn.meta.get("virtual_account", {})
-        provider = current_virtual_account_info.get("provider")
+        provider = current_virtual_account_info.get("provider", "flutterwave")
         p_s = PaymentService(provider)
         response = p_s.get_virtual_account(client_user, trxn, reference=f"{trxn.txn_ref}-regen-{generate_unique_id(5)}")
         data: dict = response.get("data", {})
